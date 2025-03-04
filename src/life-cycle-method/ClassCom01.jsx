e9e3d386b74962c4550e556146af1c2e93e57ea8
mansinghbhagaur <mansinghbhagaur1@gmail.com>
2025-03-04 11:56:51 +0530

    class components lifecycle method api fetch

--- src/life-cycle-method/ClassCom01.jsx
+++ src/life-cycle-method/ClassCom01.jsx
@@ -3,28 +3,81 @@ import React, { Component } from "react";
 class ClassCom01 extends Component {
       constructor() {
             super();
-            this.state = { count: 0 }
+            this.state = { count: 0, data: [] }
             console.log("constructor Called");
       }
 
       componentDidMount() {
-            console.log("componentDidMount Called");
+            try {
+                  const FetchData = async () => {
+                        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+                              .then((res) => res.json()).catch((err) => console.log("Error lkjhgApi", err));
+                        // console.log(response, "response")
+                        this.setState({ data: response })
+                  }
+                  FetchData();
+            } catch (error) {
+                  console.log("Error Api", "api error felled");
+            } finally {
+                  console.log("finally")
+            }
       }
 
-      componentDidUpdate() {
-            console.log("componentDidUpdate Called");
-      }
+      // componentDidUpdate() {
+      //       console.log("componentDidUpdate Called");
+      // }
 
-      componentWillUnmount() {
-            console.log("componentWillUnmount Called");
-      }
+      // componentWillUnmount() {
+      //       console.log("componentWillUnmount Called");
+      // }
 
       render() {
-            console.log("render method called")
+            // console.log(this.state.data, "data")
+            // const dataFor = this.state.data.forEach((item, i) => {
+            //       console.log(item, "foreach")
+            // })
+
+            // console.log(dataFor, "datafor")
+
+            // const dataMap = this.state.data.map((item) => item);
+            // console.log(dataMap, "dataMap")
             return (
                   <>
                         <h1>Class com 01 {this.state.count}</h1>
                         <button onClick={() => this.setState({ count: this.state.count + 1 })}>Click me</button>
+                        <h1>Print Data</h1>
+
+
+
+
+                        <table border={1}>
+                              <thead>
+                                    <tr>
+                                          <th>Id</th>
+                                          <th>Name</th>
+                                          <th>Email</th>
+                                          <th>City</th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    {
+                                          this.state.data.map((item, index) => (
+                                                <>
+                                                      <tr key={index}>
+                                                            <td>{item.id}</td>
+                                                            <td>{item.name}</td>
+                                                            <td>{item.email}</td>
+                                                            <td>{item.address.city}</td>
+                                                      </tr>
+                                                </>
+                                          ))
+                                    }
+                              </tbody>
+                        </table>
+
+
+
+
                   </>
             )
       }
