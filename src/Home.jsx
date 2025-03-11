17d0355d8d4256124ae47a532900aa7e3b5bbf90
mansinghbhagaur <mansinghbhagaur1@gmail.com>
2025-03-11 13:02:51 +0530

    custom hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Store } from './context/Store'
-import { useContext } from 'react';
+import { useCustomContextHook } from './context/Store'
+// import { Store } from './context/Store'
+// import { useContext } from 'react';
 
 const Home = () => {
-      const { data } = useContext(Store);
+      const { data } = useCustomContextHook();
+      // const { data } = useContext(Store);
       return (
             <div>
+                  <h1>Home Page</h1>
                   <h1>{data.name}</h1>
             </div>
       )
--- src/context/Store.jsx
+++ src/context/Store.jsx
@@ -1,3 +1,7 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const Store = createContext();
+
+export const useCustomContextHook = () => {
+      return useContext(Store);
+}
